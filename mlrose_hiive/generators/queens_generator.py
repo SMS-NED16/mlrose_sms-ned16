ccfc980e251960f2cb59f8b0b37db019de834823
Saad Siddiqui <saadsiddiqui.ned16@gmail.com>
2024-03-02 10:20:51 -0500

    Add maximize argument to QueensOpt

--- mlrose_hiive/generators/queens_generator.py
+++ mlrose_hiive/generators/queens_generator.py
@@ -7,5 +7,5 @@ class QueensGenerator:
     @staticmethod
     def generate(seed, size=20, maximize = False):
         np.random.seed(seed)
-        problem = QueensOpt(length=size)
+        problem = QueensOpt(length=size, maximize = maximize)
         return problem
