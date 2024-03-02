6935794860ab32bd1970a395463f517dd313dc71
Saad Siddiqui <saadsiddiqui.ned16@gmail.com>
2024-03-02 10:20:20 -0500

    Add `maximize` argument to QueensGenerator constructor
    Assignment 02 for Spring 2024 requires that all discrete optimization problems be chosen such that the fitness function is maximized. 
    
    While the `Queens` problem object accepts `maximize` as an argument, the `QueensGenerator` does not. 
    
    This is to allow use of the generator machinery while still solving a maximization problem.

--- mlrose_hiive/generators/queens_generator.py
+++ mlrose_hiive/generators/queens_generator.py
@@ -5,7 +5,7 @@ from mlrose_hiive import QueensOpt
 
 class QueensGenerator:
     @staticmethod
-    def generate(seed, size=20):
+    def generate(seed, size=20, maximize = False):
         np.random.seed(seed)
         problem = QueensOpt(length=size)
         return problem
